32451dd5866ba522010a44a92f6a573a82998d7a
jqjwhehe <114126995+jqjwhehe@users.noreply.github.com>
2022-09-24 19:25:09 +0700

    Add files via upload

---
+++ Program C++_Menentukan harga TV setelah tahun ketiga.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+	double tahun_pertama, tahun_kedua, tahun_ketiga, price;
+	
+	cout<<"Masukkan harga TV baru : Rp ";
+	cin>>price;
+	tahun_pertama = 0.85*price;
+	cout<<"Harga TV setelah tahun pertama : Rp "<<tahun_pertama<<"\n";
+	
+	tahun_kedua = 0.9*tahun_pertama;
+	cout<<"Harga TV setelah tahun kedua : Rp "<<tahun_kedua<<"\n";
+	
+	tahun_ketiga = 0.92*tahun_kedua;
+	cout<<"Harga TV setelah tahun ketiga : Rp "<<tahun_ketiga<<"\n";
+	
+	return 0;
+}
